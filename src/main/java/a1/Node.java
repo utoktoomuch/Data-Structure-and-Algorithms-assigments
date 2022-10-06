765f7ca5aaa02c359e318d6203ec4f027c82522e
utoktoomuch <bryan.r@outlook.com>
2022-10-06 16:06:46 -0400

    almost finish first question

--- src/main/java/a1/Node.java
+++ src/main/java/a1/Node.java
@@ -1,11 +1,45 @@
 package a1;
 
-public class Node {
-    private int key;
-    private double val;
+public class Node<K,E> {
+    private K k;
+    private E v;
+    private Node<K,E> next;
+    private Node<K, E> prev;
 
+    Node(K key, E val, Node<K,E> nextval){
+        k = key;
+        v = val;
+        next = nextval;
 
-    public Node(int key,double val ){
+    }
+
+    Node(Node<K,E> nextval){
+        next = nextval;
+    }
+
+    Node<K,E> next(){
+        return next;
+    }
+
+    Node<K,E> setNext(Node<K,E> nextval){
+        return next = nextval;
+    }
+
+    Node<K, E> prev() {
+        return prev;
+    }
+
+    Node<K, E> setPrev(Node<K, E> prevval){
+        return prev = prevval;
+    }
+    K k(){
+        return k;
+    }
+
+    E v(){
+        return v;
+    }
+   /* public Node(int key,double val ){
         this.key = key;
         this.val = val;
 
@@ -26,6 +60,6 @@ public class Node {
 
     public void setVal(double v){
         val = v;
-    }
+    }*/
 
 }
